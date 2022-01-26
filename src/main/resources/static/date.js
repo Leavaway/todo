e4925cc4e310cbb4fa7bb3a16edb7a3b553add8a
御坂10086 <505871488@qq.com>
2022-01-26 20:14:12 +0800

    1.26.1备份 开始使用新的UI

--- src/main/resources/static/date.js
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
-
-    <link href="https://cdn.bootcss.com/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdn.staticfile.org/font-awesome/4.7.0/css/font-awesome.css">
-            <script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
-            <script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
-            <script src="https://cdn.bootcss.com/moment.js/2.24.0/moment-with-locales.js"/>
